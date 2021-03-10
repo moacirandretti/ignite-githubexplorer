8097dc2cda3112162688e0f6935c313c0ba187e4
moacirandretti <moacir.andretti@gmail.com>
2021-03-10 20:21:12 -0300

    ADD: Chamada API do GitHub. Estilizacao com SASS. Listagem dos repos dinamicamente.

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,10 @@
+export const RepositoryItem = (props) => {
+    return (
+        <li>
+        <strong>{props.repository.name ?? 'Default'}</strong>
+        <p>{props.repository.description}</p>
+        
+        <a href={props.repository.html_url}>Acessar repositório</a>
+        </li>
+        )
+    }
