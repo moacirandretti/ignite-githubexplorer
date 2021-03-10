efb00589a792b5f5ba59872813489e09a0681801
moacirandretti <moacir.andretti@gmail.com>
2021-03-10 17:36:03 -0300

    ADD: Finalizacao da config do ambiente prod e dev com webpack e babel. Componente RepositoryList.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import './styles/global.scss'
+
 export function App(){
-    return <h1>Hello World! Rocket</h1>
+    return <h1>Hello World! UAHsuAHSua</h1>
 }
---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,16 @@
+export const RepositoryList = () => { 
+    return (
+        <section className="repository-list">
+            <h1>Lista de repositórios</h1>
+                <ul>
+                    <li>
+                        <strong>unform</strong>
+                        <p>Forms in ReactJS</p>
+
+                        <a href="#">Acessar repositório</a>
+                    </li>
+                </ul>
+            
+        </section>
+    )
+}
