5cdf6950dc22873ad8b15030231990e8ad354919
moacirandretti <moacir.andretti@gmail.com>
2021-03-10 11:56:54 -0300

    ADD: Config React para o babel.

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,8 +1,16 @@
 "use strict";
 
-var _user$address;
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.App = void 0;
 
-var user = {
-  name: "Andretti"
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var App = function App() {
+  return /*#__PURE__*/_react["default"].createElement("h1", null, "Hello Andretti!");
 };
-console.log((_user$address = user.address) === null || _user$address === void 0 ? void 0 : _user$address.street);
+
+exports.App = App;
--- src/index.js
+++
@@ -1,5 +0,0 @@
-const user = {
-    name: "Andretti"
-}
-
-console.log(user.address?.street)
